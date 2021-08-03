c609c10efd4be05da6b2fa417d0ada82c2f3c02b
yusuke-hata-code <yusuke0972@gmail.com>
2021-08-03 14:56:02 +0900

    getStaticPropsに変えた

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { getSortedPostsData } from '../lib/posts';
 import Link from "next/link"
 import Date from "../components/date"
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
   return {
     props: {
